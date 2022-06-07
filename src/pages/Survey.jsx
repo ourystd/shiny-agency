f6bf9275b947fc8e8478769906a54a6f467bdf0c
ourystd <amadoury313@gmail.com>
2022-06-07 16:30:28 +0000

    survey questions page added

--- src/pages/Survey.jsx
+++ src/pages/Survey.jsx
@@ -1,4 +1,61 @@
 import { Link, useParams } from "react-router-dom";
+import styled from "styled-components";
+
+const MainWrapper = styled.div`
+  max-width: 750px;
+  margin: 100px auto;
+  text-align: center;
+  color: #2f2e41;
+`;
+
+const QuestionNumber = styled.h2`
+  text-decoration: underline #5843e4;
+`;
+
+const QuestionText = styled.p`
+  margin: 60px auto;
+  font-size: 20px;
+  font-weight: 400;
+  line-height: 30px;
+`;
+
+const AnswersList = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 50px;
+`;
+
+const Answer = styled.button`
+  min-width: 200px;
+  min-height: 80px;
+  background-color: #f9f9fc;
+  border-radius: 30px;
+  font-size: 25px;
+  border: 1px solid transparent;
+  padding: 15px 30px;
+  margin-bottom: 60px;
+  transition: border-color 0.35s;
+
+  &:hover {
+    border-color: #5843e4;
+  }
+`;
+
+const QuestionsNavigation = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 30px;
+  font-size: 18px;
+  font-weight: 400;
+
+  a:not(:hover) {
+    color: #2f2e41;
+  }
+`;
+
+const SpaceFiller = () => <span style={{ width: 100 }} />;
 
 const Survey = () => {
   const questionNumber = parseInt(useParams().questionNumber, 10);
@@ -11,14 +68,23 @@ const Survey = () => {
   };
 
   return (
-    <div>
-      <h1>Questionnaire 🧮</h1>
-      <h2>Question #{questionNumber}</h2>
-      <p>
-        {!!prevQuestion && (
+    <MainWrapper>
+      <QuestionNumber>Question {questionNumber}</QuestionNumber>
+      <QuestionText>
+        Votre application doit-elle impérativement apparaître en premier dans
+        les résultats de recherche ?
+      </QuestionText>
+      <AnswersList>
+        <Answer>Oui</Answer>
+        <Answer>Non</Answer>
+      </AnswersList>
+      <QuestionsNavigation>
+        {!!prevQuestion ? (
           <Link style={navStyle} to={`/survey/${prevQuestion}`}>
             Précédent
           </Link>
+        ) : (
+          <SpaceFiller />
         )}
         {nextQuestion <= 10 ? (
           <Link style={navStyle} to={`/survey/${nextQuestion}`}>
@@ -29,8 +95,8 @@ const Survey = () => {
             Résultats
           </Link>
         )}
-      </p>
-    </div>
+      </QuestionsNavigation>
+    </MainWrapper>
   );
 };
 
